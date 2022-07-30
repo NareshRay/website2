03345d89a3cbb1f786a6460bacbe93a1e860eff4
NareshRay <108911738+NareshRay@users.noreply.github.com>
2022-07-30 11:05:29 +0545

    You can build you web page
    wiw it so use full

---
+++ file.js
@@ -0,0 +1,148 @@
+/*/*let a= 100;
+let b= 300;
+//let c= a+b;
+let d=a%b;
+// somthing play in console
+//console.log(c);
+//console.log(d);*/
+//for loop
+/*for (let r=0;  r<=100; r++) {
+  console.log("i hate my shyness");
+  console.log("i am very love .... ")
+  console.log("i have not tell my filling")
+  }*/
+  // while loop
+ /* let a= 0;
+  while(a<=100){
+    console.log("i hate my shyness");
+  console.log("i am very love .... ")
+  console.log("i have not tell my filling")
+ a++;
+    
+  }*/
+  //do while loop
+ /* let a=5;
+  do{
+    console.log("i hate my shyness");
+  console.log("i am very love .... ")
+  console.log("i have not tell my filling")
+ a++;
+  }while(a!=100);*/
+  // warn and error in java script
+  // warn warn in red mark of console
+  //console.warn("hello am a along pertion")
+//console.error("hello")
+//function in 
+ /*function sum(a,b){
+  c= a+b;
+  return c;
+  }
+  d=sum(654,567)
+  console.log(d);*/
+ /*function modu(a,b){
+    return d=a%b;
+  }
+  r=modu(5,2);
+  console.log(r);
+  f=modu(56,2)
+  console.log(f);*/
+//  document.alert("hello am a sarmila good boy")
+// to show any massage by use of alert
+//alert("i am a good boy");
+// some thing print in html pag
+//document.write("<h1>hello</h1>")
+//h1.style.color="#4200ff";
+ // document.innerText='hello bro';
+//  document.innerHTML='hello dost'
+//let a=document.body;
+//scarch link in file
+//let a=document.links;
+//scarch inter file of file
+//let b=document.links[2].href;
+/*let b= document.
+console.log(a);
+console.log(b);*/
+// to target id put name
+//let b= document.getElementById("hello");
+//you can see text and Html text
+//let v = b.innerText;
+//b.innerHTML="<b>hello gies</b>"
+//to change text of page
+//b.innerText="hello dost";
+//let v=b.innerText;
+
+//let v=b.innerHTML;
+//console.log(v);
+//to select class 
+//let b=document.getElementsByClassName('how');
+//let h=b.innerHTML;
+//let v=b.innerHTML;
+//console.log(h);
+//to select any text
+/*let a=document.documents.getElementsByTagName('p');
+let b=a[0].innerText;
+console.log(b);*/
+//single line selector only selecylter only select only thing if thing multi than it select top of part
+/*let a=document.querySelector('p');
+let d=a.innerText;
+d[0].style.color="#00ff15"
+console.log(d);*/
+/*let a = document.getElementsByTagName('p');
+let b = a.innerText;
+console.log(b);*/
+
+//
+// timec find in java script
+let time=new Date();
+//console.log(time.getTime());
+//console.log(time.getFullYear());
+//console.log(time.getHours());
+//console.log(time.getDay());
+//console.log(time.getMonth());
+//console.log(time.getDate());
+//console.log(time.getMinutes());
+//console.log(time.getSeconds());
+// String in java script
+//somting get and calculate
+/*function sii(){
+let a=document.getElementById('p').value;
+let b=document.getElementById('t').value;
+let c=document.getElementById('r').value;
+let si=(a*b*c)/100;
+document.getElementById('info').innerHTML=si;}*/
+// to made advanced calculatwr
+function calculate(){
+let aw=document.getElementById('a').value;
+let bw= document.getElementById('b').value;
+let sum= aw + bw;
+let dif= aw-bw;
+let mul= aw*bw;
+let div= aw/bw;
+let modu=aw%bw;
+document.getElementById('ritufriend').innerHTML= modu;
+document.getElementById('divv').innerHTML= div;
+document.getElementById('mull').innerHTML= mul;
+document.getElementById('summ').innerHTML= sum;
+document.getElementById('diff').innerHTML= dif;
+}
+
+// string in java script
+
+//let str="hello bro my name is naresh naresh "//method of write string 
+//console.log(str);//tyo print string
+//console.log(str.length)//to see length of string
+//console.log(str.indexOf("bro"))// to see going size ilof string
+//console.log(str.lastIndexOf("naresh"))//if any last string then use thea properties
+//console.log(str.slice(2));//cut thas posion value
+//console.log(str.slice(1,5));//tot one value ond print cutvalue mto mid all value
+//console.log(str.replace("naresh","ritufriend"));//to repace string value;
+//arr in java script
+//let arr= ["naresh",3,6,"hello"];//to put arr
+//console.log(arr);//
+//console.log(arr[2])//to print icjact arr
+//arr.pop()//to cut last art valilue
+//arr.shift()//to cut starting value
+//arr.unshift("hello")//to put value in start
+
+//console.log(arr.slice(2,3))//if i put 2 then it cut start 2value and start to print last bvalue
+
